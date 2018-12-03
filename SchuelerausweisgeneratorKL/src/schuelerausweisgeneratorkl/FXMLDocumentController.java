12aee16765e504305049d679e0eb29df9787e095
jonas.klocke <jonas.klocke@Lerninsel41.s-atiw.de>
2018-12-03 11:41:02 +0100

    Login Dialog...

--- SchuelerausweisgeneratorKL/src/schuelerausweisgeneratorkl/FXMLDocumentController.java
+++ SchuelerausweisgeneratorKL/src/schuelerausweisgeneratorkl/FXMLDocumentController.java
@@ -7,20 +7,25 @@ package schuelerausweisgeneratorkl;
 
 import java.io.FileNotFoundException;
 import java.net.URL;
+import java.sql.SQLException;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.beans.value.ChangeListener;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.util.Pair;
 
 /**
  *
@@ -39,7 +44,7 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private Button btnImp;
     @FXML
-    private ListView<?> lvSchueler;
+    private ListView<String> lvSchueler;
     @FXML
     private Label lblVorname;
     @FXML
@@ -64,21 +69,40 @@ public class FXMLDocumentController implements Initializable {
     private Button btnBearbeiten;
     
     private Verwaltung verwaltung;
+    int permission;
+    DB_Verbindung db;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        try {
-            verwaltung = new Verwaltung();
-            verwaltung.holeKlassennamenAusDB();
-            cbKlasse = new ComboBox<>();
-            ObservableList<String> klassen = FXCollections.observableArrayList();
-            for (String klasse : verwaltung.getKlassennamen()) {
-                klassen.add(klasse);
+        LoginDlg loginDlg = new LoginDlg();
+        Optional<Pair<String, String>> result = loginDlg.showAndWait();
+        
+        result.ifPresent(usernamePassword -> {
+            try {
+                verwaltung = new Verwaltung(usernamePassword.getKey(),usernamePassword.getValue());
+                verwaltung.holeKlassennamenAusDB();
+                System.exit(-1);
+            } catch (SQLException ex) {
+               loginDlg.setContentText("Login fehlgeschlagen");
+            }catch (Exception ex) {
+               ex.printStackTrace();
             }
-            cbKlasse.setItems(klassen);
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        });
+        //result.orElseThrow(exceptionSupplier -> );
+//        loginDlg.onCloseRequestProperty().addListener(new ChangeListener() -> {
+//            System.out.println("Handled by anonymous class listener");
+//        });
+            ObservableList<String> options = 
+            FXCollections.observableArrayList(
+                "Option 1",
+                "Option 2",
+                "Option 3"
+            );
+
+            cbKlasse = new ComboBox<>();      
+            cbKlasse.setItems(options);
+    //            cbKlasse.getItems().clear();
+    //            cbKlasse.getItems().addAll("FS161");
     }    
 
     @FXML
